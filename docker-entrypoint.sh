0e16085660c620e6a678e3539da9ed46c873d555
Ian Westcott <iwestcott@mbta.com>
2019-04-10 14:22:48 -0400

    make tile upload contingent on existence of MAPNIK_TILE_S3_BUCKET env var

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,7 +10,9 @@ sudo -H -u postgres /load_map_data.sh
 # if 'tiles' is passed as a command to run, generate and publish tiles
 if [ "$1" == "tiles" ]; then
     sudo -u postgres /var/lib/postgresql/src/generate_tiles.py
-    cd /var/lib/mod_tile/ && aws s3 sync . s3://mbta-map-tiles/ --size-only
+    if [ -n "${MAPNIK_TILE_S3_BUCKET}" ]; then
+        cd /var/lib/mod_tile/ && aws s3 sync . "s3://${MAPNIK_TILE_S3_BUCKET}/" --size-only
+    fi
 fi
 
 sudo -u postgres renderd -f -c /usr/local/etc/renderd.conf
