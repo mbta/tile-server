d3e5e0bc77f025e615775df300e50c2b2ab133b4
Maldonado <emaldonado@mbta.com>
2022-09-23 11:48:21 -0400

    fix: address issues flagged by shellcheck

--- build_push.sh
+++ build_push.sh
@@ -8,10 +8,10 @@ app=tile-server
 tag=$1
 
 # build docker image and tag it with git hash and aws environment
-docker build -t $app:$tag -t $app:latest .
+docker build -t $app:"$tag" -t $app:latest .
 
 # retrieve the `docker login` password from AWS ECR and use it
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $DOCKER_SERVER
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin "$DOCKER_SERVER"
 
 # push images to ECS image repo
-docker image push --all-tags $DOCKER_SERVER/$app
+docker image push --all-tags "$DOCKER_SERVER"/$app
