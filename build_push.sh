5f9fa01622ae56655ccdbec618bf126ebd93ff6d
Maldonado <emaldonado@mbta.com>
2022-10-21 15:00:52 -0400

    fix: fully tag Docker image so that push works

--- build_push.sh
+++ build_push.sh
@@ -8,7 +8,7 @@ app=tile-server
 tag=$1
 
 # build docker image and tag it with git hash and aws environment
-docker build -t $app:"$tag" -t $app:latest .
+docker build -t $app:"$tag" -t $app:latest -t "$DOCKER_SERVER"/$app:"$tag" -t "$DOCKER_SERVER"/$app:latest .
 
 # retrieve the `docker login` password from AWS ECR and use it
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin "$DOCKER_SERVER"
